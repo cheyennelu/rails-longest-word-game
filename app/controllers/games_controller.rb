20c6da2a71f7e5e96bd5b28813f5d91c3e6823ca
Cheyenne Lu <cheyenne.lu@gmail.com>
2021-02-16 16:41:32 +0800

    style check

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,21 +7,27 @@ class GamesController < ApplicationController
     @letters = Array.new(10) { charset.sample }
   end
 
-  def score
-    word = params["word"]
+  def english_word?(word)
     raw_json = open("https://wagon-dictionary.herokuapp.com/#{word}").read
     json_hash = JSON.parse(raw_json)
-    letters = params["letters"]
+    json_hash['found'] == true
+  end
 
-    word_check = word.upcase.chars.all? do |char|
+  def word_check(word, letters)
+    word.upcase.chars.all? do |char|
       letters.include?(char) && word.count(char) <= letters.count(char)
     end
+  end
 
-    if json_hash["found"] == true && word_check == true
+  def score
+    word = params['word']
+    letters = params['letters']
+
+    if english_word?(word) == true && word_check(word, letters) == true
       @word_result = "#{word.upcase} is a good word!" # the variable is just container. logic will run in controller first
-    elsif json_hash["found"] == false
+    elsif english_word?(word) == false
       @word_result = "Sorry #{word.upcase} is not an English word!"
-    elsif word_check == false
+    elsif word_check(word, letters) == false
       @word_result = "Sorry #{word.upcase} cannot be built out of #{letters}!"
     end
   end
